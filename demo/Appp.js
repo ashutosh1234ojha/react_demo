ccc2c4a26466bc1f3dcbf555760907c53c9a5138
Ashutosh ojha <Kunal.Sharma@arris.com>
2021-03-24 23:59:23 +0530

    set  handled

---
+++ demo/Appp.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const Appp = () => {
+  const [name, setName]=useState('Ashu')
+  const [session, setSession]=useState({number:6,title:'as'})
+  const [count, setCount]=useState(0)
+
+
+ const clickListener=()=>{
+
+  setName('ojha')
+  setSession({number:7,title:'our'})
+  setCount(count=>count+1)
+  }
+  return (<View style={style.body}>
+    <Text style={style.text}>{name}</Text>
+    <Text style={style.text}>This is number {session.number} and text is {session.title}</Text>
+    <Text style={style.text}>Count is {count} </Text>
+    <Button title='click' onPress={clickListener}></Button>
+
+  </View>);
+};
+
+const style=StyleSheet.create({
+  body:{
+    flex:1,
+    backgroundColor:'#f0ff8f',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  text:{
+    color:'#00f000',
+    fontSize:30,
+    margin:14
+
+      }
+});
+export default Appp;
