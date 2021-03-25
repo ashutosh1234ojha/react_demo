a901958214961dfa398b4bae077739bff3b07448
Ashutosh ojha <Kunal.Sharma@arris.com>
2021-03-25 19:27:18 +0530

    style

--- demo/Appp.js
+++ demo/Appp.js
@@ -29,6 +29,7 @@ const Appp = () => {
   }
   return (<View style={style.body}>
     <Text style={style.text}>{name}</Text>
+    <Text style={{color:'#f05f80',fontSize:34}}>Tom</Text>
     <Text style={style.text}>This is number {session.number} and text is {session.title}</Text>
     <Text style={style.text}>Count is {count} </Text>
     <Button title='click' onPress={clickListener}></Button>
@@ -44,9 +45,14 @@ const style=StyleSheet.create({
     justifyContent:'center',
   },
   text:{
+    flex:1,
     color:'#00f000',
     fontSize:30,
-    margin:14
+    margin:14,
+    borderColor:'#fff34f',
+    borderWidth:3,
+    textTransform:'uppercase'
+
 
       }
 });
